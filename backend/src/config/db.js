0ba727f175a3246792afd59bd1afac5d486521d5
iwana994994 <iwana994994@gmail.com>
2025-08-14 17:06:18 +0200

    again and

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,8 +6,7 @@ dotenv.config();
 export const connectDB = async () => {
 try {
     await mongoose.connect(process.env.MONGO_URI,{
-      useNewUrlParser: true,
-       useUnifiedTopology: true,
+      
        serverSelectionTimeoutMS: 20000 
     });
     console.log('❤  MongoDB connected successfully! ❤');
