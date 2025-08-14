e400b2ea75b07e174be96738efaa5794e1cfaa4c
iwana994994 <iwana994994@gmail.com>
2025-08-14 16:58:09 +0200

    daa

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,7 +5,9 @@ dotenv.config();
 
 export const connectDB = async () => {
 try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI,{
+      serverSelectionTimeoutMS: 20000
+    });
     console.log('❤  MongoDB connected successfully! ❤');
     
 } catch (error) 
