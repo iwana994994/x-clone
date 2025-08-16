2c565b4bd8b90eb6c9ec97389fbda220569e0416
iwana994994 <iwana994994@gmail.com>
2025-08-16 14:23:56 +0200

    mmm

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -26,3 +26,39 @@ export const createPost = async (req, res) => {
     res.status(500).json({ error: "Server error" });
   }
 };
+export const getUserPosts = async (req, res) => {
+  const { username } = req.params;
+
+  const user = await User.findOne({ username });
+  if (!user) return res.status(404).json({ error: "User not found" });
+
+  const posts = await Post.find({ user: user._id })
+    .sort({ createdAt: -1 })
+    .populate("user", "username firstName lastName profilePicture")
+    
+    };
+
+export const getPost = async (req, res) => {
+  const { postId } = req.params;
+
+  const post = await Post.findById(postId)
+    .populate("user", "username firstName lastName profilePicture")
+    .populate({
+      path: "comments",
+      populate: {
+        path: "user",
+        select: "username firstName lastName profilePicture",
+      },
+    });
+
+  if (!post) return res.status(404).json({ error: "Post not found" });
+
+  res.status(200).json({ post });
+};
+export const getPosts = async (req, res) => {
+  const posts = await Post.find()
+    .sort({ createdAt: -1 })
+    .populate("user", "username firstName lastName profilePicture");
+
+  res.status(200).json({ posts });
+};
--- backend/src/routes/post.route.js
+++ backend/src/routes/post.route.js
@@ -4,6 +4,9 @@ import { createPost } from "../controllers/post.controller.js";
 const route = express.Router();
 
 route.post ("/",createPost)
+route.get("/", getPosts);
+route.get("/:postId", getPost);
+route.get("/user/:username", getUserPosts);
 
 
 export default route;
