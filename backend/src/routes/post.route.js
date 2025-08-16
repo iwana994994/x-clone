6727d628c90cf02d16ce455ebecf7af314e122d8
iwana994994 <iwana994994@gmail.com>
2025-08-16 12:41:42 +0200

    k

--- backend/src/routes/post.route.js
+++ backend/src/routes/post.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createPost, getPosts } from "../controllers/post.controller.js";
+import { createPost } from "../controllers/post.controller.js";
 
 const route = express.Router();
 
