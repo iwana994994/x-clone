5eb92b88dc4a099a50453a7be15cf578248ab229
iwana994994 <iwana994994@gmail.com>
2025-08-16 14:33:49 +0200

    ee

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -34,3 +34,12 @@ const clerkUser = await clerkClient.users.getUser(userId);
 
   res.status(201).json({ user, message: "User created successfully" });
 }
+
+export const getCurrentUser = asyncHandler(async (req, res) => {
+  const { userId } = getAuth(req);
+  const user = await User.findOne({ clerkId: userId });
+
+  if (!user) return res.status(404).json({ error: "User not found" });
+
+  res.status(200).json({ user });
+});
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -5,5 +5,6 @@ import { requireAuth } from '@clerk/express';
 const route= express.Router();
 
 route.post("/sync",requireAuth(),userSync)
+router.get("/me",requireAuth(), getCurrentUser);
 
 export default route;
