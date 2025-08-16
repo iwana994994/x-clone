e815f2cbbe05c6da90cb7b7280eebc8474d8a8a2
iwana994994 <iwana994994@gmail.com>
2025-08-16 12:36:30 +0200

    s

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -1,27 +1,27 @@
 import Post from "../models/post.model.js";
 import User from "../models/user.model.js";
-import { getAuth } from "@clerk/express";
-
-export const createPost =async (req, res) => {
-  const { userId } = getAuth(req);
-  const { content } = req.body;
- 
+export const createPost = async (req, res) => {
+  try {
+    const { userId } = getAuth(req);
+    const { content } = req.body;
 
     if (!content) {
-    return res.status(400).json({ error: "Post must contain text" });
+      return res.status(400).json({ error: "Post must contain text" });
+    }
+
+    const user = await User.findOne({ clerkId: userId });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const post = await Post.create({
+      user: user._id,
+      content,
+    });
+
+    res.status(201).json({ post });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
   }
-  
-  const user = await User.findOne({ clerkId: userId });
-  if (!user) return res.status(404).json({ error: "User not found" });
-
-  
-  const post = await Post.create({
-    user: user._id,
-    content: content || "",
-    
-  });
-
-  res.status(201).json({ post });
-
-}
-
+};
