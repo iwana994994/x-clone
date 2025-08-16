7b121037686e29e3613f5c41503eb5168da939b3
iwana994994 <iwana994994@gmail.com>
2025-08-16 14:38:37 +0200

    ddd

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -5,6 +5,6 @@ import { requireAuth } from '@clerk/express';
 const route= express.Router();
 
 route.post("/sync",requireAuth(),userSync)
-router.get("/me",requireAuth(), getCurrentUser);
+route.get("/me",requireAuth(), getCurrentUser);
 
-export default route;
+export default route;``
